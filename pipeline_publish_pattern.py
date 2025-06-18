be5b9974655fe9ea34a773052b10c8f44ad26e85
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2025-06-18 08:24:21 +0000

    Cleaned up

---
+++ pipeline_publish_pattern.py
@@ -0,0 +1,26 @@
+import requests
+import click
+
+def retrieve_pattern_data_from_file(path:str):
+    try:
+        with open(path, 'r') as file:
+            content = file.read()
+            return content
+    except Exception as err:
+        print(f"An error occurred {err}")
+
+def send_pattern_to_calm_hub(pattern):
+    requests.post("http://localhost:8080/calm/namespaces/ossf2025/patterns", data=pattern, headers={'Content-Type': 'application/json'})
+
+@click.command()
+@click.option('-p', help="Path to Pattern file to upload")
+def main(p):
+    if p is None:
+        click.echo("Please provide a path to the pattern you would like to instantiate ⚠️")
+    else:
+        pattern_contents = retrieve_pattern_data_from_file(p)
+        send_pattern_to_calm_hub(pattern_contents)
+        click.echo("Pattern successfully uploaded to CALM Hub ✅")
+
+if __name__ == "__main__":
+    main()
---
+++ pipeline_validate_and_publish_architecture.py
@@ -0,0 +1,48 @@
+import subprocess
+import click
+import json
+import requests
+
+class ArchitectureValidationException(Exception):
+    pass
+
+def validate(pattern, architecture):
+    result = subprocess.run([f"calm validate -p {pattern} -a {architecture}"], capture_output=True, text=True, shell=True)
+    output = json.loads(result.stdout)
+    if(output['hasErrors'] or output['hasWarnings']):
+        click.echo("Validation failed for {architecture} ❌. Errors found: \n")
+        pretty_json = json.dumps(output, indent=4)
+        print(pretty_json)
+        raise ArchitectureValidationException()
+    else:
+        click.echo("Architecture Validated Successfully ✅")
+
+def retrieve_architecture_file(path):
+    try:
+        with open(path, "r") as file:
+            content = file.read()
+            return content
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def upload_to_calm_hub(architecture_path):
+    architecture = retrieve_architecture_file(architecture_path)
+
+    response = requests.post("http://localhost:8080/calm/namespaces/ossf2025/architectures", data=architecture, headers={'Content-Type': 'application/json'})
+    if(response.status_code == 201):
+        click.echo("Successfully uploaded architecture to CALM Hub ✅")
+    else:
+        click.echo("Failed to upload architecture to CALM Hub❌")
+
+@click.command()
+@click.option('-p', help="Pattern file to validate against")
+@click.option('-a', help="Architecture file to publish")
+def main(p, a):
+    try:
+        validate(p,a)
+        upload_to_calm_hub(a)
+    except Exception as err:
+        pass
+
+if __name__ == "__main__":
+    main()
