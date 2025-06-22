0050c9808ae3e9cc77937461364f79e1b9ee5132
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2025-06-22 17:29:56 +0100

    Add namespace

--- pipeline_publish_pattern.py
+++ pipeline_publish_pattern.py
@@ -9,17 +9,18 @@ def retrieve_pattern_data_from_file(path:str):
     except Exception as err:
         print(f"An error occurred {err}")
 
-def send_pattern_to_calm_hub(pattern):
-    requests.post("http://localhost:8080/calm/namespaces/ossf2025/patterns", data=pattern, headers={'Content-Type': 'application/json'})
+def send_pattern_to_calm_hub(pattern, namespace):
+    requests.post(f"http://localhost:8080/calm/namespaces/{namespace}/patterns", data=pattern, headers={'Content-Type': 'application/json'})
 
 @click.command()
 @click.option('-p', help="Path to Pattern file to upload")
-def main(p):
+@click.option('-n', help="Namespace to save the pattern to")
+def main(p, n):
     if p is None:
         click.secho("Please provide a path to the pattern you would like to publish to CALM Hub ⚠️", fg="yellow")
     else:
         pattern_contents = retrieve_pattern_data_from_file(p)
-        send_pattern_to_calm_hub(pattern_contents)
+        send_pattern_to_calm_hub(pattern_contents, n)
         click.secho("Pattern successfully uploaded to CALM Hub ✅", fg="green")
 
 if __name__ == "__main__":
